4cba9f7660c0c31017746b04b0d3a83293e4bc7a
815382636 <815382636@qq.com>
2020-12-22 14:04:33 +0800

    delete all

--- scrapy_cerebrovascular/translate_cd.py
+++
@@ -1,7 +0,0 @@
-from scrapy_cerebrovascular.pg_connection import new_connection
-from scrapy_cerebrovascular.scrapy_html import ScrapyHtml
-
-engine, session = new_connection()
-sh = ScrapyHtml(engine, session)
-sh.translate()
-print('translate succeed !')
